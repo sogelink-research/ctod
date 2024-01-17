d6a0e469764f7c6de0d2a82cee11ae559cca6e6a
tebben <ebben.tim@gmail.com>
2024-01-17 06:53:53 +0100

    fix https://github.com/sogelink-research/ctod/issues/1

--- ctod/core/cog/cog.py
+++ ctod/core/cog/cog.py
@@ -1,19 +1,30 @@
-import time
+from morecantile import TileMatrixSet
+from rio_tiler.io import Reader
+from rio_tiler.models import ImageData
 
-from rio_tiler.io import COGReader
 
-def download_tile(tms, x, y, z, geotiff_path, resampling_method="bilinear"):
+def download_tile(tms: TileMatrixSet, x: int, y: int, z: int, geotiff_path: str, resampling_method="bilinear") -> ImageData:
+    """Retrieve an image from a Cloud Optimized GeoTIFF based on a tile index.
+
+    Args:
+        tms (TileMatrixSet): The tile matrix set to use.
+        x (int): x tile index.
+        y (int): y tile index.
+        z (int): z tile index.
+        geotiff_path (str): Path or URL to the Cloud Optimized GeoTIFF.
+        resampling_method (str, optional): RasterIO resampling algorithm. Defaults to "bilinear".
+
+    Returns:
+        ImageData: _description_
     """
-    Retrieve an image from a Cloud Optimized GeoTIFF based on a tile index.
-    """
 
-    with COGReader(geotiff_path, tms=tms) as src:     
-        data = src.tile(tile_z=z, tile_x=x, tile_y=y, resampling_method=resampling_method)
+    with Reader(geotiff_path, tms=tms) as src:     
+        image_data = src.tile(tile_z=z, tile_x=x, tile_y=y, resampling_method=resampling_method)
         
         # Set nodata to 0 if nodata is present in the metadata
-        nodata = src.info().nodata_value if hasattr(src.info(), 'nodata_value') else None
+        nodata_value = src.info().nodata_value if hasattr(src.info(), 'nodata_value') else None
 
-        if nodata is not None:
-            data.data[data.data == nodata] = float(0)
+        if nodata_value is not None:
+            image_data.data[image_data.data == nodata_value] = float(0)
 
-    return data
+    return image_data
